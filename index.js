83c71dc4741181959185e5bb6d3ce64fddcf43f2
Michal Krawczykowski <krawczy5@coventry.ac.uk>
2019-10-09 15:52:45 +0100

    index welcome test

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello Node.js World");
+//import koa
+var Koa = require('koa');
+//import koa-router which is used to route user request to its path
+var Router = require('koa-router');
+//import koa-body parser whic his used to extract parameters from requests
+
+//create a koa instance and store it in app variable
+var app = new Koa();
+
+var router = new Router();
+
+router.get('/api/v1.0', welcomeAPI);
+
+//use the root routes
+app.use(router.routes());
+
+//run the werver on port 3000
+app.listen(3000);
+function welcomeAPI(cnx, next){
+
+cnx.body = {message:'Welcome to Oktob API version 1.0'};
+}
