75b2048416823c47ec43cdd02addba93adfccb50
Michal Krawczykowski <krawczy5@coventry.ac.uk>
2019-10-10 10:59:29 +0100

    signup page done

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 
@@ -10,6 +11,7 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={Signup} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- app/src/containers/Signup.js
+++ app/src/containers/Signup.js
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
+import "./Signup.css";
 
 export default function Login(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [retypePassword, setreTypePassword] = useState("");
 
   function validateForm() {
-    return username.length > 0 && password.length > 0;
+    return username.length > 0 && password.length > 0 && retypePassword.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+function validatePassword() {
+    if(password !== retypePassword)
+        alert("Passwords are not matching");
   }
 
   return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
+    <div className="Signup">
+      <form onSubmit={validatePassword}>
         <FormGroup controlId="username" bsSize="large">
           <ControlLabel>Username</ControlLabel>
           <FormControl
@@ -34,8 +36,16 @@ export default function Login(props) {
             type="password"
           />
         </FormGroup>
+        <FormGroup controlId="retypepassword" bsSize="large">
+          <ControlLabel>Retype Password</ControlLabel>
+          <FormControl
+            value={retypePassword}
+            onChange={e => setreTypePassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
         <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
+          Sign up
         </Button>
       </form>
     </div>
