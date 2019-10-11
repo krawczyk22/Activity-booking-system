c42bdd223828a25e7aca23fb37ed377ca162d7e7
Michal Krawczykowski <krawczy5@coventry.ac.uk>
2019-10-11 12:32:03 +0100

    login api is working

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var Router = require('koa-router');
 var model = require('../models/user.js');
-var sha256 = require('js-sha256');
+var crypto = require('crypto');
 
 var router = Router({
    prefix: '/api/v1.0/users'
@@ -23,23 +23,23 @@ router.get('/get/:username', async (cnx, next) =>{
 
 //note that we have injected the body parser only in the POST request
 router.post('/insert/', bodyParser(), async (cnx, next) =>{
-   var hash = sha256.create();
-   hash.update(cnx.request.body.password);
-   hash.hex();
-   let newUser = {username:cnx.request.body.username, password:hash
-   };
+   var hash = crypto.createHash('sha256').update(cnx.request.body.password).digest('hex');
+   let newUser = {username:cnx.request.body.username, password:hash};
+   console.log(newUser);
    await model.addUser(newUser);
    cnx.body = {message:"added successfully"};
 });
 
 //note that we have injected the body parser only in the POST request
 router.post('/login/', bodyParser(), async (cnx, next) =>{
-   var hash = sha256.create();
-   hash.update(cnx.request.body.password);
-   hash.hex();
-   await model.loginCheck(cnx.request.body.username);
-   console.log({password:hash});
-   //cnx.body = {message:"added successfully"};
+   var password = crypto.createHash('sha256').update(cnx.request.body.password).digest('hex');
+   let result = await model.loginCheck(cnx.request.body.username);
+   if(result[0].password == password) {
+      cnx.body = {message:"logged in"};
+   }
+   else {
+      cnx.body = {message:"not logged in"};
+   }
 });
 
 module.exports = router;
