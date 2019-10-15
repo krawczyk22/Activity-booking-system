d17d05ee058175e540dd819287ea75ca29d22fef
Michal Krawczykowski <krawczy5@coventry.ac.uk>
2019-10-15 21:42:02 +0100

    signup page connected to backend

--- app/src/containers/Signup.js
+++ app/src/containers/Signup.js
@@ -1,53 +1,173 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React from "react";
 import "./Signup.css";
+import { Form, Input, Alert, Button } from "antd";
 
-export default function Login(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [retypePassword, setreTypePassword] = useState("");
+class RegistrationForm extends React.Component {
+  state = {
+    confirmDirty: false,
+    addedSucessfully: false, //if the user is added successfully
+    showSuccess: false, //if should we show a successful feedback message after adding a user
+    showError: false, //if should we show an error feedback message after adding a
+    errorCode: 400, //to save the errorCode we recieved from the api server
+    responseStatus: "nothing", //the validation status of the email
+    errorMessage: "" //the error message to display to the user after server rejects action
+    };
 
-  function validateForm() {
-    return username.length > 0 && password.length > 0 && retypePassword.length > 0;
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        //echo the values to the browser console to make sure they are correct
+        console.log('Received values of form: ', values);
+        //here we should send a request to our server to post the user
+        //use fetch API to post the user data
+        fetch('http://localhost:3000/api/v1.0/users/insert/', { 
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json', 
+          },
+          body: JSON.stringify({values}) 
+        }).then(res => {
+          if(res.ok) 
+            this.setState({addedSucessfully:true})
+          else 
+            this.setState({
+              addedSucessfully:false,
+              errorCode: res.status
+            });
+          return res.json()
+        }).then(data => this.checkResponse(data))
+      } 
+    });
+  };
+
+  handleEmail = ()=> {
+    this.setState({responseStatus:"nothing"}) 
   }
 
-function validatePassword() {
-    if(password !== retypePassword)
-        alert("Passwords are not matching");
-  }
+  handleConfirmBlur = e => { 
+    const { value } = e.target;
+    this.setState({ 
+      confirmDirty: this.state.confirmDirty || !!value 
+    }); 
+  };
 
-  return (
-    <div className="Signup">
-      <form onSubmit={validatePassword}>
-        <FormGroup controlId="username" bsSize="large">
-          <ControlLabel>Username</ControlLabel>
-          <FormControl
-            autoFocus
-            type="username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <FormGroup controlId="retypepassword" bsSize="large">
-          <ControlLabel>Retype Password</ControlLabel>
-          <FormControl
-            value={retypePassword}
-            onChange={e => setreTypePassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Sign up
-        </Button>
-      </form>
-    </div>
-  );
+  compareToFirstPassword = (rule, value, callback) => { 
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!'); 
+    } else {
+      callback(); 
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => { 
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true }); }
+    callback(); 
+  };
+
+  checkResponse = (data) => {
+    if(this.state.addedSucessfully){ 
+      this.props.form.resetFields(); 
+      this.setState({
+        showSuccess:true,
+        showError : false 
+      });
+    } else {
+      //handle errors
+      this.setState({ 
+        errorMessage: data.message, 
+        showSuccess:false, 
+        showError : true, 
+        responseStatus: "error"
+      }); 
+    }
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    //this code will handle form responsivness on small devices
+    const formItemLayout = {
+      labelCol: { 
+        xs: { span: 24 },
+        sm: { span: 8 },
+      }, 
+      wrapperCol: { 
+        xs: { span: 24 },
+        sm: { span: 16 },
+        }, 
+      };
+      const tailFormItemLayout = { 
+        wrapperCol: {
+          xs: { 
+            span: 24,
+            offset: 0, 
+          },
+          sm: { 
+            span: 16,
+            offset: 8, 
+          },
+        }, 
+      };
+
+    //prefix the email input with some decoration
+    return ( 
+      <div className="Signup">
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        <Form.Item label="Username" hasFeedback>
+          {getFieldDecorator('username', { 
+            rules: [
+              {
+              required: true,
+              message: 'Please input your username!', },
+            ],
+          })(<Input onChange={this.handleEmail} />
+          )}
+          </Form.Item>
+          <Form.Item label="Password" hasFeedback>
+            {getFieldDecorator('password', { 
+              rules: [
+                {
+                required: true,
+                message: 'Please input your password!', 
+                },
+                {
+                min: 6,
+                message: 'password should be at least 6 characters long!', },
+                {
+                validator: this.validateToNextPassword,
+                },
+              ],
+            })(<Input.Password />)}
+          </Form.Item>
+          <Form.Item label="Confirm Password" hasFeedback>
+            {getFieldDecorator('passwordConfirmation', {
+              rules: [
+                {
+                required: true,
+                message: 'Please confirm your password!',
+                },
+                {
+                validator: this.compareToFirstPassword,
+                },
+              ],
+            })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+          {this.state.showSuccess ? <Alert message="account created successfully" type="success" /> :null}
+          {this.state.showError ? <Alert message={this.state.errorMessage} type="error" /> :null}
+        </Form>
+      </div>
+    );
+  }
 }
+const Signup = Form.create({ name: 'register' })(RegistrationForm);
+export default Signup;
